dca5dd4926571a9f019a91b231009d7a1d644927
Hadassah Uche <uhadassah1@gmail.com>
2023-11-17 22:13:47 +0200

    im not sure how i should go about this

--- UcheHadassah6_3/HadassahUche6_3a.cpp
+++ UcheHadassah6_3/HadassahUche6_3a.cpp
@@ -2,3 +2,18 @@
 by outputting the appropriate message: This is a palindrome accordingly. A 
 word is a palindrome if the letters spell the same message when written both forward 
 and backward. For example: madam, anna, bob, noon are palindromes.*/
+#include<iostream>
+#include<cstring>
+using namespace std;
+const int MAX = 100;
+int main()
+{
+	char word[MAX];
+	int i = 0, j = 0;
+	cout << "Enter a word:";
+	cin >> word;
+	while (word[i] != word[j])
+	{
+
+	}
+}
