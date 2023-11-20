fd0db5045c20899504a547b46e2032230c12faac
Hadassah Uche <uhadassah1@gmail.com>
2023-11-20 22:32:44 +0200

    Cleaned up the code and added comments

--- UcheHadassah6_3/HadassahUche6_3a.cpp
+++ UcheHadassah6_3/HadassahUche6_3a.cpp
@@ -1,7 +1,5 @@
-/*Palindromes: a) Write a program which reads a word and checks if it is a palindrome 
-by outputting the appropriate message: This is a palindrome accordingly. A 
-word is a palindrome if the letters spell the same message when written both forward 
-and backward. For example: madam, anna, bob, noon are palindromes.*/
+/*Name:Uche hadassah
+This program reads a word and checks if it is a palindrome*/
 #include<iostream>
 #include<cstring>
 using namespace std;
@@ -10,10 +8,12 @@ int main()
 {
 	char word[MAX];
 	int i = 0, j;
+
 	cout << "Enter a word:";
 	cin >> word;
-	//I will add a condition to take a word not a string
-	for (j = 0; word[j] != '\0'; j++);//Gets the length of the word
+
+	for (j = 0; word[j] != '\0'; j++);//Gets the length of the word, same as "strlen(word);"
+
 	while (i <= j-1)
 	{
 		if (word[i] != word[j-1])
@@ -24,11 +24,12 @@ int main()
 		if (i == j-1)
 		{
 			cout << "This is a palindrome";
+			break;
 		}
 		i++;
 		j--;
 	}
-	if (i > j - 1)
+	if (i > j - 1)//When we have an even number of words
 	{
 		cout << "This is a palindrome";
 	}
